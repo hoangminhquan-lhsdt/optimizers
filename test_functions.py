26ecd94891432401e7b0f83674e924facb870dd3
laohacsieudeptrai <laohacsieudeptrai@gmail.com>
2021-02-26 17:08:28 +0700

    fix Rosenbrock function

--- test_functions.py
+++ test_functions.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 class Rosenbrock:
-	def __init__(self):
+	def __init__(self, a=1, b=5):
 		self.minima = [1, 1]
+		self.a = 1
+		self.b = 5
 	def f(self, x, y):
-		return (1 - x)**2 + 5*(y - x**2)**2
+		return (self.a - x)**2 + self.b*(y - x**2)**2
 	def df(self, x, y):
-		return np.array((2*(10*(x**3) - 10*x*y + x - 1), 10*(y - x**2)))
+		return np.array((2*(2*self.b*(x**3) - 2*self.b*x*y + x - self.a), 2*self.b*(y - x**2)))

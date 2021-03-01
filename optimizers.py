dd6a623a85a2e01b5991d98c3fb3291f26f6591b
laohacsieudeptrai <laohacsieudeptrai@gmail.com>
2021-03-01 10:13:11 +0700

    added Adagrad

--- optimizers.py
+++ optimizers.py
@@ -1,12 +1,25 @@
 import numpy as np
-# put desired test function name here
+
+
+class SGD:
+	def __init__(self, F, lr=0.001):
+		self.name = 'Stochastic Gradient Descent'
+		self.func = F
+		self.lr = lr
+	def step(self, x, y):
+		g = self.func.df(x, y)
+		return x - self.lr*g[0], y - self.lr*g[1]
+
 
 class Momentum:
-	'''Implementation of SGD with Momentum from "On the Momentum Term in Gradient Descent Learning Algorithms"
+	'''Implementation of SGD with Momentum from "On the Momentum Term in Gradient Descent Learning Algorithms".
 
 	Arguments:
-		lr (float, optional): learning rate
-		momentum (float, optional): momentum term
+	----------
+	lr : float, optional
+		learning rate (default: 0.001)
+	momentum : float, optional
+		momentum term (default: 0.9)
 	'''
 	def __init__(self, F, lr=0.001, momentum=0.9):
 		self.name = 'SGD with Momentum'
@@ -21,28 +34,46 @@ class Momentum:
 
 
 class AdaGrad:
-	'''Implementation of Adam from "Adam: A Method for Stochastic Optimization"
+	'''Implementation of AdaGrad from "Adaptive Subgradient Methods for
+Online Learning and Stochastic Optimization".
 
 	Arguments:
-		a (float, optional): alpha
-		b1 (float, optional): beta1
-		b2 (float, optional): beta2
-		eps (float, optional): epsilon
+	----------
+	lr : float, optional
+		learning rate (default: 0.001)
+	lr_decay : float, optional
+		learning rate decay (default: 0)
+	weight_decay : float, optional
+		weight decay (default: 0)
+	eps : float, optional
+		epsilon
 	'''
-	def __init__(self):
-		pass
+	def __init__(self, F, lr=0.01, eps=1e-10):
+		self.name = 'Adagrad'
+		self.func = F
+		self.lr = lr
+		self.eps = eps
+		self.sq_grad = np.zeros(2)  # sum of squared gradient
 	def step(self, x, y):
-		pass
+		g = self.func.df(x, y)
+		self.sq_grad += g**2
+		v = self.lr * g / (np.sqrt(self.sq_grad)+ self.eps)
+		return x - v[0], y - v[1]
 
 
 class Adam:
-	'''Implementation of Adam from "Adam: A Method for Stochastic Optimization"
+	'''Implementation of Adam from "Adam: A Method for Stochastic Optimization".
 
 	Arguments:
-		a (float, optional): alpha
-		b1 (float, optional): beta1
-		b2 (float, optional): beta2
-		eps (float, optional): epsilon
+	----------
+	a   : float, optional
+		alpha, learning rate
+	b1  : float, optional
+		beta2, exponential decay for moving averages of gradient
+	b2  : float, optional
+		beta1, exponential decay for moving averages of squared gradient
+	eps : float, optional
+		epsilon
 	'''
 	def __init__(self, F, a=0.001, b1=0.9, b2=0.999, eps=1e-8):
 		self.name = 'Adam'
@@ -62,4 +93,5 @@ class Adam:
 		self.v = self.b2*self.v + (1-self.b2)*g_t*g_t
 		m_hat = self.m / (1 - self.b1**self.t)
 		v_hat = self.v / (1 - self.b2**self.t)
-		return (x - (self.a*m_hat[0] / (np.sqrt(v_hat[0]) + self.eps)), y - (self.a*m_hat[1] / (np.sqrt(v_hat[1]) + self.eps)))
+		v = self.a*m_hat / (np.sqrt(v_hat) + self.eps)
+		return (x - v[0], y - v[1])

990d384b704f635457e276421041706fdfe6aae9
Crues Sun <nguyenngoclannhu@gmail.com>
2021-03-08 11:34:24 +0700

    linear function that adam do not converge, err: converge too slow

--- test_functions.py
+++ test_functions.py
@@ -10,3 +10,67 @@ class Rosenbrock:
 	def df(self, x, y):
 		return np.array((2*(2*self.b*(x**3) - 2*self.b*x*y + x - self.a), 2*self.b*(y - x**2)))
 
+class linear_func:
+	def __init__(self, a,b, k):
+		self.minima = -1;
+		self.a = a
+		self.b = b
+		self.k = k
+		self.b1 = 0.9
+		self.b2 = 0.99#1/(1 + self.C**2)
+		self.t = 0
+		self.m = 0
+		self.v = 0
+		self.max_v = 0
+	def f(self,x,t):
+		if t % self.k == 1:
+			return self.a*x
+		return -self.b*x
+	def fs(self,x,t):
+		return [self.f(xs,ts) for xs,ts in zip(x,t)]
+	def df(self,x,t):
+		if t % self.k == 1:
+			return self.a
+		return -self.b
+	def Adam(self,x,lr,eps,amsgrad = False):
+		self.t += 1
+		g_t = self.df(x,self.t-1)
+		self.m = self.b1 * self.m + (1-self.b1)*g_t
+		self.v = self.b2 * self.v + (1-self.b2)*g_t*g_t
+
+		m_hat = self.m / (1 - self.b1**self.t)
+
+		# if amsgrad:
+		# 	self.max_v = max(self.v,self.max_v)
+		# 	denom = np.sqrt(self.max_v) / (np.sqrt(1 - self.b2**self.t) + eps)
+		# else:
+		# 	denom = np.sqrt(self.v) / (np.sqrt(1 - self.b2 **self.t) + eps)
+		if amsgrad:
+			self.max_v = max(self.v,self.max_v)
+			v_hat = self.max_v / (1 - self.b2**self.t)
+			return x - lr*m_hat/ (np.sqrt(v_hat) + eps)
+
+		v_hat = self.v / (1 - self.b2**self.t)
+		return x - lr*m_hat/ (np.sqrt(v_hat) + eps)
+
+		# return x - lr*self.m / denom
+	def AMSGrad(self,x,lr,eps):
+		self.t += 1
+		g_t = self.df(x,self.t)
+		self.m = self.b1*self.m + (1 - self.b1)*g_t
+		self.v = self.b2*self.v + (1- self.b2)*g_t*g_t
+
+		# step_size = lr / (1 - self.b1**self.t)
+		# v_hat = self.v / (np.sqrt(self.v) + eps)
+		self.max_v = max(self.v,self.max_v)
+		denom = np.sqrt(self.max_v) / (np.sqrt(1 - self.b2**self.t) + eps)
+
+		return x - lr* self.m / denom
+	def reset(self):
+		self.t = 0
+		self.m = 0
+		self.v = 0
+		self.max_v = 0
+
+
+
--- visualize.py
+++ visualize.py
@@ -3,26 +3,43 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 
 from optimizers import Momentum as Opt
-from test_functions import Rosenbrock as Func
+from test_functions import linear_func as Func
 
-X = np.arange(-2.5,2.1,0.1)
-Y = np.arange(-2,3.1,0.1)
-X, Y = np.meshgrid(X, Y)
-func = Func()
-Z = func.f(X, Y)
+# X = np.arange(-2.5,2.1,0.1)
+# Y = np.arange(-2,3.1,0.1)
+# X, Y = np.meshgrid(X, Y)
+func = Func(1010,10,101)
+# Z = func.f(X, Y)
+# Y = func.f(X,[_ for _ in range(100)])
 
 fig = plt.figure()
-ax = plt.axes(xlim=(-2.5, 2), ylim=(-2, 3))
-ax.contourf(X, Y, Z, 50)
-ax.plot(func.minima[0], func.minima[1], 'ro', label='Global Minima')
+# ax = plt.axes(xlim=(-2.5, 2), ylim=(-2, 3))
+ax = plt.axes()
+# ax.contourf(X, Y, Z, 50)
+# ax.plot(func.minima[0], func.minima[1], 'ro', label='Global Minima')
 
-x = [-2,-1]
-Optimer = Opt(a = 0.001)
-maxIter = 7500
-for _ in range(maxIter):
-	x = Optimer.step(x)
-	print(x[0],x[1],func.f(x[0],x[1]))
+# x = [-2,-1]
+# Optimer = Opt(a = 0.001)
+# maxIter = 7500
+# for _ in range(maxIter):
+# 	x = Optimer.step(x)
+# 	print(x[0],x[1],func.f(x[0],x[1]))
 
-plt.plot(x[0],x[1],'yo')
-plt.legend()
+x = 0
+maxIter = 25000
+x_t = []
+for t in range(maxIter):
+	x = func.Adam(x,0.001,1e-8,amsgrad = True)
+	# print(x)
+	if x > 1:
+		x = 1
+	elif x < -1:
+		x = -1
+	# print(func.f(x,t+1))
+	x_t.append(x)
+
+# ax.plot(range(maxIter),func.fs(x_t,range(maxIter)),'yo')
+ax.plot(range(maxIter),x_t,'y')
+# ax.plot(0,x_t[0],'yo')
+# plt.legend()
 plt.show()
